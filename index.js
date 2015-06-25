b9cfe2b478b7373ed2cd18406687c45714740b76
Austin Wood <i@austinwood.me>
2015-06-24 19:07:27 -0600

    Node can read in message from Reids across a socket

---
+++ index.js
@@ -0,0 +1,18 @@
+const io = require('socket.io').listen(process.env.PORT || 3000);
+const redis = require('redis');
+
+const clientSub = redis.createClient();
+
+io.on('connection', function (socket) {
+  console.log('Someone has connected');
+
+  socket.on('disconnect', function () {
+    console.log('Someone has disconnected');
+  });
+});
+
+clientSub.subscribe('main_channel');
+
+clientSub.on('message', function (channel, message) {
+  console.log(message);
+});
